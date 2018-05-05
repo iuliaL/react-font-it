e3defbca73cdd41ef006d05ee6f85c572438e333
iulia <iuliamarialungu@yahoo.com>
2018-05-05 17:30:05 +0300

    named component wrapper

--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,7 @@ function ReactFontIt<TOriginalProps extends {}>(
 	WrappedComponent: React.SFC<TOriginalProps>,
 	config: Config
 ): React.SFC<TOriginalProps> {
-	return function (props: TOriginalProps): JSX.Element {
+	return function FontIt(props: TOriginalProps): JSX.Element {
 		if (!config.google && !config.file) {
 			throw Error('no fonts configured for react-font-face');
 		} else {
@@ -71,7 +71,5 @@ function ReactFontIt<TOriginalProps extends {}>(
 				</div>
 			);
 		}
-
-
 	};
 }
